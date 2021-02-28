1144ead2451123d12c5ca2134d2d28ac314bf7d9
Wister <e@wister.mx>
2021-02-28 00:53:38 -0600

    Initial Commit

---
+++ kubeteiner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  kubeteiner
+//
+//  Created by Eddy Wister on 27/02/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
